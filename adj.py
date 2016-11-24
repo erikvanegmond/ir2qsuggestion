794d567aad5a575d44e9ef492ab073e4fc18ca3c
erikvanegmond <Bailpzt06>
2016-11-24 12:08:36 +0100

    Added levenshtein feature

--- adj.py
+++ adj.py
@@ -1,19 +1,15 @@
 from collections import Counter
 from collections import defaultdict
 
+from ranker import Ranker
 from sessionizer import Sessionizer
 import numpy as np
 
 
-class ADJ:
-    def __init__(self, sessions_file="../data/test_session"):
-        sessionizer = Sessionizer(data_path=sessions_file)
-        self.sessions = sessionizer.get_sessions()
-        self.cooccurrences = defaultdict(list)
-
-    def adj_function(self, anchor_query, norm=True):
-        if anchor_query in self.cooccurrences:
-            return self.cooccurrences[anchor_query]
+class ADJ(Ranker):
+    def adj_function(self, anchor_query):
+        if anchor_query in ADJ.cooccurrences:
+            return ADJ.cooccurrences[anchor_query]
 
         cooccurrence_list = Counter()
         for session in self.sessions:
@@ -26,11 +22,10 @@ class ADJ:
         top20 = cooccurrence_list.most_common(20)
         top20_queries = [x for x, y in top20]
 
-        if norm:
-            tot = sum([y for x, y in top20])
-            top20_freq = [y / float(tot) for x, y in top20]
-        else:
-            top20_freq = [y for x, y in top20]
+        tot = sum([y for x, y in top20])
+        top20_relfreq = [y / float(tot) for x, y in top20]
+        top20_absfreq = [y for x, y in top20]
 
-        self.cooccurrences[anchor_query] = top20_queries, top20_freq
-        return top20_queries, top20_freq
+        ADJ.cooccurrences[anchor_query] = {'adj_queries': top20_queries, 'absfreq': top20_absfreq,
+                                           'relfreq': top20_relfreq}
+        return ADJ.cooccurrences[anchor_query]
