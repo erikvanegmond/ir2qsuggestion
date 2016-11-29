892b2ed896eb348d965dcfb304fbf5e45c41358a
Celeste Kettler <celestekettler@gmail.com>
2016-11-29 22:08:12 +0100

    the batch file counts now

--- lm_call.bat
+++ lm_call.bat
@@ -1,7 +1,9 @@
 @echo off
 cls
+set /a Counter=0
 :start
 echo off
 C:\Users\cel_w\Anaconda2\python.exe C:\Users\cel_w\Documents\GitHub\ir2qsuggestion\lambda_mart.py
-pause
+set /a Counter+=1
+echo %Counter%
 goto start
