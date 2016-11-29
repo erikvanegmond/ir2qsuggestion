19188e869e0aed477bd0f98ee9a58526f28f44a5
Jaimy <jaimyTvandijk@gmail.com>
2016-11-29 17:23:04 +0100

    Created HRED feature

---
+++ features/HRED.py
@@ -0,0 +1,21 @@
+import cPickle as pickle
+from features.feature import Feature
+
+class DatasetFeature(Feature):
+    features = []
+
+    def __init__(self, data_file="../data/HRED_features.pkl"):
+        if not len(DatasetFeature.features):
+            DatasetFeature.features = pickle.load(open(data_file, 'rb'))
+        else:
+            print "Features already loaded."
+
+class HRED(DatasetFeature):
+
+    @staticmethod
+    def calculate_feature(compared_query, queries):
+        fts = []
+        for q in queries:
+            fts.append(DatasetFeature.features[compared_query][q])
+            Feature.cooccurrences[compared_query]['HRED'] = DatasetFeature.features[compared_query][q]
+        return fts
---
+++ test.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from model import Model
+from sessionizer import Sessionizer
+import cPickle as pickle
+import numpy as np
+
+snizer = Sessionizer()
+start_time = datetime.now()
+time = start_time.strftime('%d-%m %H:%M:%S')
+print("[%s: Loading sessions...]" % time)
+with open('../data/aug_test_session.pkl', 'rb') as f:
+    test_sessions = pickle.load(f)
+print("[Loaded %s test sessions. It took %f seconds.]" % (len(test_sessions), (datetime.now() - start_time).seconds))
+
+start_time = datetime.now()
+time = start_time.strftime('%d-%m %H:%M:%S')
+print("[%s: Loading model...]" % time)
+m = Model.load('../models/29-11_4.589_0_90005x1000x90005.npz')
+print("[It took %d seconds.]" % ((datetime.now() - start_time).seconds))
+
+start_time = datetime.now()
+time = start_time.strftime('%d-%m %H:%M:%S')
+print("[%s: Calculating loss...]" % time)
+losses = []
+for session in test_sessions:
+    X_test = session[:-1]
+    y_test = session[1:]
+    loss = m.calculate_loss(X_test, y_test)
+    losses.append(loss)
+print("[It took %d seconds.]" % ((datetime.now() - start_time).seconds))
+
+print("Mean loss: %s" % (np.mean(losses)))
+print("Stddev loss: %s" % (np.std(losses)))
