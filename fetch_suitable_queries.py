ddc272722482a1d211195690b70018bb658c7de2
Jaimy T. van Dijk <jaimyvandijk@hotmail.com>
2016-12-02 18:01:09 +0100

    Added code to fetch the validation data as well

--- fetch_suitable_queries.py
+++ fetch_suitable_queries.py
@@ -1,9 +1,11 @@
 from features.adj import ADJ
+from features.ranker import Ranker
 from datetime import datetime
 
 start = datetime.now()
 time = start.strftime('%d-%m %H:%M:%S')
 print('[%s: Creating ADJ...]' % time)
+ranker = Ranker(train_sessions_file='../data/val_session')
 adj = ADJ()
 print('[It took %s seconds.]' % (start - datetime.now()).seconds)
 
