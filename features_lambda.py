6b8f822ba33311428edc08b2b8f16e0239de9f7c
Celeste Kettler <celestekettler@gmail.com>
2016-12-18 23:04:41 +0100

    for only making long_tail features

--- features_lambda.py
+++ features_lambda.py
@@ -24,13 +24,13 @@ def aug_data(data):
         auged_data.append(tmp.astype(np.int32))
     return auged_data
 
-sessions = lm.adj.find_suitable_sessions("../data/lm_val_sessions.pkl")
+
 # Do LambdaMart for 3 different scenario's
 # 1 Next-QueryPrediction (when anchor query exists in background data)
 # for each session:
 def create_next_query_csv():
     print('[Creating dataset for next_query predictions.]')
-    experiment_string = "next_query_val_short"
+    experiment_string = "next_query_val"
     print("Performing experiment: " + experiment_string)
     lm.next_query_prediction(sessions, experiment_string)
     print("---" * 30)
@@ -49,14 +49,19 @@ def create_noisy_query_csv():
 # 3 Long-TailPrediction (when the anchor is not present in the background data)
 # train, val and test set retain sessions for which the anchor query has not been
 # seen in the background set (long-tail query)
-def create_longtail_query_csv():
-    experiment_string = "long_tail_val"
+def create_longtail_query_csv(sessions1, experiment_string):
+    # experiment_string = "long_tail_val"
     print("Performing experiment: " + experiment_string)
-    lm.make_long_tail_set(sessions, experiment_string)
+    lm.make_long_tail_set(sessions1, experiment_string)
     print("---" * 30)
     lm.hred.save()
 
-print('[Creating validation features.]')
-create_next_query_csv()
+print('[Creating long_tail features.]')
+# create_next_query_csv()
 # create_noisy_query_csv()
-# create_longtail_query_csv()
+sessions_tr = lm.adj.find_suitable_sessions("../data/lm_train_sessions.pkl")
+create_longtail_query_csv(sessions=sessions_tr,experiment_string="long_tail_train")
+sessions_val = lm.adj.find_suitable_sessions("../data/lm_val_sessions.pkl")
+create_longtail_query_csv(sessions=sessions_val,experiment_string="long_tail_val")
+sessions_test = lm.adj.find_suitable_sessions("../data/lm_test_sessions.pkl")
+create_longtail_query_csv(sessions=sessions_test,experiment_string="long_tail_test")
